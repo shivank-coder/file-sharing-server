33a9830a2be98dc02218edada8e3328583da5ee2
Mayank Singh <amanjh05@gmail.com>
2023-10-12 01:00:00 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    const MONGO_URI = `mongodb+srv://file:123@file-connect.a2puw9e.mongodb.net/fileSharing?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
